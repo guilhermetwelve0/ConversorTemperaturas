ed0574226886220aa72c2c4badfcff7ee944f8db
guilhermetwelve0 <rlacabral4@gmail.com>
2021-04-03 18:53:39 -0300

    Add project files.

---
+++ ConversorTemperaturas/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorTemperaturas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double c, f, k;
+
+            Console.WriteLine("### CONVERSOR DE TEMPERATURAS");
+            Console.Write("Insira a temperatura em Celsius: ");
+            c = double.Parse(Console.ReadLine());
+            Console.WriteLine("--------------------------------------------");
+
+
+            //( c * 9 / 5) + 32 = F
+            f = (c * 9 / 5) + 32;
+
+            // c + 273,15
+            k = c + 273.15;
+
+            Console.WriteLine(c + " graus celsius " + f + " graus fahrenheit ");
+            Console.WriteLine(c + " graus celsius " + k + " graus kelvin ");
+            Console.WriteLine("--------------------------------------------");
+            Console.ReadKey();
+        }
+    }
+}
